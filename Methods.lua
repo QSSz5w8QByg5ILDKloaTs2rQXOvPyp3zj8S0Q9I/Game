f151b9d16343087cf96891e7f98290997c5efc9f
QSSz5w8QByg5ILDKloaTs2rQXOvPyp3zj8S0Q9I <Lennybon@proton.me>
2025-09-26 14:41:35 +0300

    Create Methods.lua

---
+++ Methods.lua
@@ -0,0 +1,15 @@
+return {
+    FromRGB = Color3.fromRGB,
+
+    Vector3New = Vector3.new,
+    Vector3Zero = Vector3.zero,
+    Vector2New = Vector2.new,
+
+    CFrameNew = CFrame.new,
+    CFrameAngles = CFrame.Angles,
+
+    MathRandom = math.random,
+    MathFloor = math.floor,
+    MathMassive = math.huge, -- massive? you know what else is massive?
+    MathClamp = math.clamp,
+}
