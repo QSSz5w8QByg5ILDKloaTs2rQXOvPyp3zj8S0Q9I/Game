cf1d4998d22782c3e4988940a1deb44d307a05bb
QSSz5w8QByg5ILDKloaTs2rQXOvPyp3zj8S0Q9I <Lennybon@proton.me>
2025-09-26 13:55:08 +0300

    Create Services.lua

---
+++ Services.lua
@@ -0,0 +1,9 @@
+return {
+    Workspace = game:GetService('Workspace'),
+    Players = game:GetService('Players'),
+    ReplicatedStorage = game:GetService('ReplicatedStorage'),
+
+    RunService = game:GetService('RunService'),
+    UserInputService = game:GetService('UserInputService'),
+    TeleportService = game:GetService('TeleportService')
+}
